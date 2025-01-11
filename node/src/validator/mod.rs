74f5e48bb7d2ae7b4a997120c375e17410cc06fc
kaxxa123 <axzammit@hotmail.com>
2025-01-11 15:40:47 +0100

    added Consensus to Validator

--- node/src/validator/mod.rs
+++ node/src/validator/mod.rs
@@ -2,19 +2,30 @@ use crate::traits::NodeInterface;
 
 use aleo_std::StorageMode;
 use anyhow::{anyhow, Result};
+use parking_lot::Mutex;
 use snarkos_lite_account::Account;
+use snarkos_lite_node_bft::{
+    helpers::init_primary_channels, ledger_service::CoreLedgerService, spawn_blocking,
+};
+use snarkos_lite_node_consensus::Consensus;
 use snarkvm::prelude::{block::Block, store::ConsensusStorage, Ledger, Network};
 use std::{
     net::SocketAddr,
     sync::{atomic::AtomicBool, Arc},
 };
+use tokio::task::JoinHandle;
 
 #[derive(Clone)]
 pub struct Validator<N: Network, C: ConsensusStorage<N>> {
     /// The ledger of the node.
     ledger: Ledger<N, C>,
-    // /// The consensus module of the node.
-    // consensus: Consensus<N>,
+    /// The consensus module of the node.
+    consensus: Consensus<N>,
+
+    /// The spawned handles.
+    handles: Arc<Mutex<Vec<JoinHandle<()>>>>,
+    /// The shutdown signal.
+    shutdown: Arc<AtomicBool>,
 }
 
 impl<N: Network, C: ConsensusStorage<N>> Validator<N, C> {
@@ -36,6 +47,23 @@ impl<N: Network, C: ConsensusStorage<N>> Validator<N, C> {
         // Initialize the ledger.
         let ledger: Ledger<N, C> = Ledger::load(genesis, storage_mode.clone())?;
 
+        // Initialize the ledger service.
+        let ledger_service = Arc::new(CoreLedgerService::new(ledger.clone(), shutdown.clone()));
+
+        // Initialize the consensus.
+        let trusted_validators: [SocketAddr; 0] = [];
+        let mut consensus = Consensus::new(
+            account.clone(),
+            ledger_service.clone(),
+            bft_ip,
+            &trusted_validators,
+            storage_mode.clone(),
+        )?;
+        // Initialize the primary channels.
+        let (primary_sender, primary_receiver) = init_primary_channels::<N>();
+        // Start the consensus.
+        consensus.run(primary_sender, primary_receiver).await?;
+
         Err(anyhow!("This is an error message"))
     }
 
@@ -51,21 +79,21 @@ impl<N: Network, C: ConsensusStorage<N>> NodeInterface<N> for Validator<N, C> {
     async fn shut_down(&self) {
         info!("Shutting down...");
 
-        // // Shut down the node.
-        // trace!("Shutting down the node...");
-        // self.shutdown
-        //     .store(true, std::sync::atomic::Ordering::Release);
+        // Shut down the node.
+        trace!("Shutting down the node...");
+        self.shutdown
+            .store(true, std::sync::atomic::Ordering::Release);
 
-        // // Abort the tasks.
-        // trace!("Shutting down the validator...");
-        // self.handles.lock().iter().for_each(|handle| handle.abort());
+        // Abort the tasks.
+        trace!("Shutting down the validator...");
+        self.handles.lock().iter().for_each(|handle| handle.abort());
 
         // // Shut down the router.
         // self.router.shut_down().await;
 
-        // // Shut down consensus.
-        // trace!("Shutting down consensus...");
-        // self.consensus.shut_down().await;
+        // Shut down consensus.
+        trace!("Shutting down consensus...");
+        self.consensus.shut_down().await;
 
         info!("Node has shut down.");
     }
