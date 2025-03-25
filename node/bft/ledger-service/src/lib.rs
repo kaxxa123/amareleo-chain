e82d622c20c531f4b19ed7fe06abc4f7c5114dc0
kaxxa123 <axzammit@hotmail.com>
2025-03-25 16:32:06 +0100

    api-shutdown: cleanup dead code ProverLedgerService

--- node/bft/ledger-service/src/lib.rs
+++ node/bft/ledger-service/src/lib.rs
@@ -28,11 +28,6 @@ pub mod mock;
 #[cfg(feature = "mock")]
 pub use mock::*;
 
-#[cfg(feature = "prover")]
-pub mod prover;
-#[cfg(feature = "prover")]
-pub use prover::*;
-
 pub mod traits;
 pub use traits::*;
 
